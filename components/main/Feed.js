9038a79a02e942f5ee0ba8fa3e3445c0eee70c2c
Bill <billlouis.bl@gmail.com>
2023-06-15 15:01:46 +0800

    Camera Halfway done

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {View, Text} from 'react-native'
-export default function feed() {
+export default function Feed() {
   return (
     <View>
         <Text> Feeds</Text>
