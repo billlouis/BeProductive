3a9bf797cc65fa562fa2a85812cf0eaab9d32cd7
kris27oen <chris27owen@gmail.com>
2023-06-16 00:15:48 +0800

    first navbar design

--- components/Main.js
+++ components/Main.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 import {View,Text} from 'react-native'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Icon from '@mdi/react'
+import { mdiHomeOutline, mdiSetCenter, mdiSolid } from '@mdi/js';
+import { mdiBellOutline } from '@mdi/js';
+import { mdiCalendarBlankOutline } from '@mdi/js';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {connect} from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchUser } from '../redux/actions/index'
 
 import FeedScreen from './main/Feed'
-
 import ProfileScreen from './main/Profile'
+import NotifScreen from './main/Notification_tab'
+import AgendaScreen from './main/Agenda_tab'
+import { shadow } from 'react-native-paper';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 const EmptyScreen = () =>{
     return(null)
 }
@@ -21,31 +26,48 @@ export class main extends Component {
     }
     render() {
         return (
-            <Tab.Navigator initialRouteName='Feed' labeled = {false}>
-                <Tab.Screen name="Feed" component={FeedScreen}
-                    options={{
-                    tabBarIcon: ({color,size}) => (
-                        <MaterialCommunityIcons name = "home" color={color} size ={26}/>
-                    )
-                }}/>
-                <Tab.Screen name="AddContainer" component={EmptyScreen}
-                    listeners={({navigation})=> ({
-                        tabPress: event=>{
+            <Tab.Navigator 
+                initialRouteName="Feed" 
+                labeled = {false} 
+                screenOptions={({route}) => ({
+                    tabBarShowLabel: false,
+                    tabBarStyle: {
+                        backgroundColor: "#3BE2B0", 
+                        width: 230, 
+                        height: 70,
+                        alignSelf: "center",
+                        marginBottom: 50,
+                        borderRadius: 50,
+                        borderColor: "white",
+                        borderWidth: 2,
+                        shadowColor: "black",
+                        shadowRadius: 10,
+                        shadowOffset: {width: 0, height: 4},
+                        shadowOpacity: 0.6
+                    },  
+                    tabBarIcon: () => {
+                        let iconName, rn = route.name;
+
+                        if(rn === "Feed") iconName = mdiHomeOutline;
+                        else if(rn === "Notification") iconName = mdiBellOutline;
+                        else if(rn === "Agenda") iconName = mdiCalendarBlankOutline;
+
+                        return <Icon path = {iconName} color="white" size ={1.3}/>
+                    },
+                })}
+                
+            >
+
+                <Tab.Screen name="Feed" component={FeedScreen}/>
+                <Tab.Screen name="Notification" component={NotifScreen}
+                    listeners={({navigation}) => ({tabPress: event=>{
                             event.preventDefault();
                             navigation.navigate("Add")
                         }
                     })}
-                    options={{
-                    tabBarIcon: ({color,size}) => (
-                        <MaterialCommunityIcons name = "camera" color={color} size ={26}/>
-                    )
-                }}/>
-                <Tab.Screen name="Profile" component={ProfileScreen}
-                    options={{
-                    tabBarIcon: ({color,size}) => (
-                        <MaterialCommunityIcons name = "account" color={color} size ={26}/>
-                    )
-                }}/>
+                />
+                <Tab.Screen name="Agenda" component={AgendaScreen}/>
+
             </Tab.Navigator>
         )
     }
---
+++ components/main/Agenda_tab.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+export default function Agenda() {
+  return (
+    <View>
+        <Text> Agenda</Text>
+    </View>
+  )
+}
