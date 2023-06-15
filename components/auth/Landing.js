488a8efafa10ee95bd6d9259bcca5bc45235507f
Bill <billlouis.bl@gmail.com>
2023-06-15 13:35:28 +0800

    work yayy

--- components/auth/Landing.js
+++ components/auth/Landing.js
@@ -3,7 +3,7 @@ import {Text,View,Button} from 'react-native'
 
 export default function Landing({navigation}) {
   return (
-    <View style ={{flex : 1, justifyContent: 'Center'}}>
+    <View style ={{flex : 1, justifyContent: 'center'}}>
         <Button title ="Register" onPress={() => navigation.navigate("Register")}/>
         <Button title ="Login" onPress={() => navigation.navigate("Login")}/>
     </View>
